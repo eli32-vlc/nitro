bd171decdc40266ea239f05c0b8c45dc4119e122
Eason Li (zenith) <84105075+eli32-vlc@users.noreply.github.com>
2023-12-31 12:36:02 +1000

    Create main.py

---
+++ main.py
@@ -0,0 +1,11 @@
+from discord_webhook import DiscordWebhook
+import time
+
+while 0==0:
+	code = "Wk7O7aEnPD7yJsaQl0IAhLJ1J2Q6rZ6gE7qVkXGkr-x_qVZQD_2KQgP_9gGZvqdbUmWIY7eYbFFEJn0i9BePZIVAY7mvpUXMzFKjUj7j1DLbgZ0D-qu8KjYWrcba_ASPwq71h_TrkZzga40dpCgflg12N5GLNQYupZFBa_NJaaLFSQEn7fvokHInc6gJMKzLKyQem0kBo5GrIc9K8LDaOoc0k7Xap__9359YOhlARYrvY9k82xg3eDk5Ylpm8eE.RtXKlhU_PFUR5dXs_io9Bw"
+	shuffled_code = ''.join(random.sample(code, len(code)))
+
+	code = "https://discord.com/billing/partner-promotions/1180231712274387115/eyJhbGciOiJkaXIiLCJlbmMiOiJBMjU2R0NNIn0.."+shuffled_code
+	print(" ")
+	print(code)
+	
